b1d39094e1c580ed926fb53846efca2e4ce045b5
tazV2 <tjkhan22@outlook.com>
2016-11-15 00:37:27 -0800

    Update mas2.js

--- mastering/mas2.js
+++ mastering/mas2.js
@@ -1,11 +1,15 @@
-var gr =[22, 22, 22];
+var gr =[22, 22, 22, , 22];
 var sum = 0;
+var count = 0;
 
 if (gr.length > 0){
     for (i = 0; i < gr.length; i++){
+        if (gr[i] != undefined){
         sum += gr[i];
+        count++
     }
-    document.write(sum/gr.length);
+    }
+    document.write(sum/count);
 }
 
 else 
